b287941b173d524461619760bdb3a018bf0173e1
Dong Yuwei <yuwei.dong@rea-group.com>
2015-02-26 21:22:55 +0800

    delete all the words which do not have translation

--- test/extract_translation.js
+++ test/extract_translation.js
@@ -16,27 +16,34 @@ var translation = {};
 list.forEach(function(word,i){
     (function(word){
         db.get(word, function (err, value) {
-          // if (err) return console.log('Ooops!', err)
+            if(err){
+                console.log('error: ', err)
+            }
 
-          // console.log('name=' + value);
-          translation[word] = value;
-          length = length - 1;
+            if(value && JSON.parse(value)['ok'] === 0){
+                console.log(word, value)
+                db.del(word, function(err){
+                    
+                })
+            }
+          // translation[word] = value;
+          // length = length - 1;
 
-          if(length === 0){
-            fs.writeFileSync('all_translation.json', JSON.stringify(translation), 'utf-8');
+          // if(length === 0){
+          //   fs.writeFileSync('all_translation.json', JSON.stringify(translation), 'utf-8');
 
-            var top10000 = extractTopWords(data,translation,10000);
-            fs.writeFileSync('top10000_translation.json', JSON.stringify(top10000), 'utf-8');
+          //   var top10000 = extractTopWords(data,translation,10000);
+          //   fs.writeFileSync('top10000_translation.json', JSON.stringify(top10000), 'utf-8');
 
-            var top20000 = extractTopWords(data,translation,20000);
-            fs.writeFileSync('top20000_translation.json', JSON.stringify(top20000), 'utf-8');
+          //   var top20000 = extractTopWords(data,translation,20000);
+          //   fs.writeFileSync('top20000_translation.json', JSON.stringify(top20000), 'utf-8');
 
-            var top30000 = extractTopWords(data,translation,30000);
-            fs.writeFileSync('top30000_translation.json', JSON.stringify(top30000), 'utf-8');
+          //   var top30000 = extractTopWords(data,translation,30000);
+          //   fs.writeFileSync('top30000_translation.json', JSON.stringify(top30000), 'utf-8');
 
-            var top50000 = extractTopWords(data,translation,50000);
-            fs.writeFileSync('top50000_translation.json', JSON.stringify(top50000), 'utf-8');
-          }
+          //   var top50000 = extractTopWords(data,translation,50000);
+          //   fs.writeFileSync('top50000_translation.json', JSON.stringify(top50000), 'utf-8');
+          // }
         })
     })(word);
     
