45ec7fc96fcdf888d3ddde36a05b97915b4d5a29
Dong Yuwei <yuwei.dong@rea-group.com>
2015-02-26 16:25:53 +0800

    extract translation of top N words

--- test/extract_translation.js
+++ test/extract_translation.js
@@ -8,6 +8,10 @@ var json = fs.readFileSync('../dictionary/google_227800_words.json', 'utf-8');
 var data = JSON.parse(json);
 
 var list = Object.keys(data), length = list.length;
+list.sort(function(a, b){
+    return data[a]- data[b];
+});
+
 var translation = {};
 list.forEach(function(word,i){
     (function(word){
@@ -20,8 +24,31 @@ list.forEach(function(word,i){
 
           if(length === 0){
             fs.writeFileSync('all_translation.json', JSON.stringify(translation), 'utf-8');
+
+            var top10000 = extractTopWords(data,translation,10000);
+            fs.writeFileSync('top10000_translation.json', JSON.stringify(top10000), 'utf-8');
+
+            var top20000 = extractTopWords(data,translation,20000);
+            fs.writeFileSync('top20000_translation.json', JSON.stringify(top20000), 'utf-8');
+
+            var top30000 = extractTopWords(data,translation,30000);
+            fs.writeFileSync('top30000_translation.json', JSON.stringify(top30000), 'utf-8');
+
+            var top50000 = extractTopWords(data,translation,50000);
+            fs.writeFileSync('top50000_translation.json', JSON.stringify(top50000), 'utf-8');
           }
         })
     })(word);
     
 });
+
+
+function extractTopWords(data,translation,limit){
+    var list = Object.keys(data);
+    var mapping = {};
+    for(var i=0;i<limit;i++){
+        mapping[list[i]] = translation[list[i]];
+    }
+
+    return mapping;
+}
