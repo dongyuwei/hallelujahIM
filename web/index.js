d2698c069b849e4c2e4d0bdc2aee14214cc2e2e3
Yuwei <yuwdong@thoughtworks.com>
2017-08-30 15:52:19 +0800

    start http server and read/update preference

---
+++ web/index.js
@@ -0,0 +1,37 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        preference: {
+            showTranslation: true,
+            pinyinApi: 'baidu'
+        }
+    },
+    methods: {
+        getPreference: function (argument) {
+            var self = this;
+            fetch('http://localhost:62718/preference')
+            .then(function (res) {
+                return res.json()
+            })
+            .then(function (preference) {
+                console.log(preference);
+                self.preference = preference;
+            })
+        },
+        updatePreference: function () {
+            fetch('http://localhost:62718/preference', {
+                method: 'POST',
+                body: JSON.stringify(this.preference)
+            })
+            .then(function (res) {
+                return res.json()
+            })
+            .then(function (preference) {
+                console.log("updated preference:", preference);
+            })
+        }
+    }
+})
+
+app.getPreference();
+
