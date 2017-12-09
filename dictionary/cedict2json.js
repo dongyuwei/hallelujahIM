24dfff5a30a865451c819f0b818d93efa5cb9c3e
Dong Yuwei <newdongyuwei@gmail.com>
2017-12-09 22:36:20 +0800

    sort pinyin/hanzi/english by Google pinyin dictionary frequency

--- dictionary/cedict2json.js
+++ dictionary/cedict2json.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
+const assert = require('assert');
 
 const content = fs.readFileSync('./cedict_1_0_ts_utf-8_mdbg.txt', 'utf-8');
 const lines = content.split('\n');
 let results = {};
 
-const googlePinyin = fs.readFileSync('./google_pinyin_rawdict_utf16_65105_freq.txt', 'utf-8');
+const googlePinyin = fs.readFileSync('./google_pinyin_rawdict_utf16_65105_freq.txt', 'ucs2');
 const googlePinyinLines = googlePinyin.split('\n');
 let googlePinyinFrequency = {};
 googlePinyinLines.forEach(function(line){
@@ -21,17 +22,58 @@ lines.forEach(function(line){
         const originalPinyin = arr[1];
         const pinyin = originalPinyin.replace(/\d|\s/g, '').toLowerCase();
         const translations = arr[2].split(/\//g).filter((text => text.trim() !== ""));
-        if (!results[pinyin]) {
-            results[pinyin] = [cn].concat(translations);
-        } else {
-            if (results[pinyin].indexOf(cn) === -1) {
-                results[pinyin] = results[pinyin].concat([cn].concat(translations));
-            } else {
-                results[pinyin] = results[pinyin].concat(translations);
-            }
-        }
+        results[pinyin] = (results[pinyin] || []).concat({
+            cn: cn,
+            translations: translations
+        });
     }
 });
 
+let finalResults = {};
+for (let pinyin in results) {
+    results[pinyin].sort(function(a, b){
+        return (googlePinyinFrequency[b.cn] || 0) - (googlePinyinFrequency[a.cn] || 0);
+    });
 
-fs.writeFileSync('cedict.json', JSON.stringify(results, null, 3), 'utf-8');
+    results[pinyin].forEach((item) => {
+        if (!finalResults[pinyin]) {
+            finalResults[pinyin] = [item.cn].concat(item.translations);
+        } else {
+            if (finalResults[pinyin].indexOf(item.cn) === -1) {
+                finalResults[pinyin] = finalResults[pinyin].concat(item.cn).concat(item.translations);
+            } else {
+                finalResults[pinyin] = finalResults[pinyin].concat(item.translations);
+            }
+        }
+    });
+}
+
+assert.deepEqual(finalResults['ceshi'], [ '测试',
+    'to test (machinery etc)',
+    'to test (students)',
+    'test',
+    'quiz',
+    'exam',
+    'beta (software)',
+    '侧室',
+    'sideroom',
+    'concubine',
+    '策士',
+    'strategist',
+    'counsellor on military strategy',
+    '策试',
+    'imperial exam involving writing essay on policy 策論|策论' ]);
+
+assert.deepEqual(finalResults['gaoji'], [ '高级',
+    'high level',
+    'high grade',
+    'advanced',
+    'high-ranking',
+    '告急',
+    'to be in a state of emergency',
+    'to report an emergency',
+    'to ask for emergency assistance',
+    '搞基',
+    '(slang) to engage in male homosexual practices' ]);
+
+fs.writeFileSync('cedict.json', JSON.stringify(finalResults, null, 3), 'utf-8');
