86e676c86817c34f81722a5393f869e37bfb5d17
Dong Yuwei <newdongyuwei@gmail.com>
2018-07-11 14:55:55 +0800

    declare CONFIGURATION_BUILD_DIR to set the build output directory

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-xcodebuild -workspace hallelujah.xcworkspace/ -scheme hallelujah -configuration Release
+xcodebuild -workspace hallelujah.xcworkspace/ -scheme hallelujah -configuration Release CONFIGURATION_BUILD_DIR=/tmp/hallelujah/build/release
