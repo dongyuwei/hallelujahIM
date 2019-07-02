f627a35fab4d95f884206c312843866e0f3800cc
Dong Yuwei <newdongyuwei@gmail.com>
2019-07-02 16:19:08 +0800

    install/activate/select hallelujah IME

--- dev.sh
+++ dev.sh
@@ -1,5 +1,8 @@
+echo "Usage: sudo sh dev.sh"
 mkdir -p ${HOME}/.hallelujah/debug
 xcodebuild -workspace hallelujah.xcworkspace/ -scheme hallelujah -configuration Release CONFIGURATION_BUILD_DIR=${HOME}/.hallelujah/debug
 pkill -9 hallelujah
 sudo rm -rf  /Library/Input\ Methods/hallelujah.app/
 sudo cp -R ${HOME}/.hallelujah/debug/hallelujah.app /Library/Input\ Methods/hallelujah.app
+sudo /Library/Input\ Methods/hallelujah.app/Contents/MacOS/hallelujah --install
+echo "hallelujah IME is installed and activated. Wait a moment to use it..."
