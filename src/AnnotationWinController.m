896ef56f609da76105e2c115fd34a59fca5e1c8c
Dong Yuwei <newdongyuwei@gmail.com>
2019-07-08 14:40:22 +0800

    to support dark mode appearance, no special logic needed if we did not write the wrong/stupid code [NSColor colorWithCalibratedRed:0.133f green:0.152f blue:0.172f alpha:1.0f]; rgb value range is 0~1, not 0~255.

--- src/AnnotationWinController.m
+++ src/AnnotationWinController.m
@@ -24,23 +24,7 @@ static AnnotationWinController *sharedController;
 
     [self.panel setStyleMask:NSWindowStyleMaskBorderless];
     [self.panel setOpaque:YES];
-
-    if ([self isDarkMode]) {
-        NSColor *bgColor = [NSColor colorWithCalibratedRed:0.133f green:0.152f blue:0.172f alpha:1.0f];
-        [self.panel setBackgroundColor:bgColor];
-        [self.view setBackgroundColor:bgColor];
-        [self.label setBackgroundColor:bgColor];
-        NSColor *textColor = [NSColor colorWithCalibratedRed:1.0f green:1.0f blue:1.0f alpha:1.0f];
-        [self.view setTextColor:textColor];
-        [self.label setTextColor:textColor];
-    }
-
     [self hideWindow];
-}
-
-- (BOOL)isDarkMode {
-    NSString *interfaceStyle = [NSUserDefaults.standardUserDefaults valueForKey:@"AppleInterfaceStyle"];
-    return [interfaceStyle isEqualToString:@"Dark"];
 }
 
 - (void)showWindow:(NSPoint)origin {
