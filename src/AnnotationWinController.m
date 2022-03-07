d30fe186c415784ab1db8617f553e376f6afd27a
Benjamin <benjaminx@gmail.com>
2022-03-07 21:37:46 +0800

    Fix more spaces translate view issues.

--- src/AnnotationWinController.m
+++ src/AnnotationWinController.m
@@ -18,28 +18,26 @@ static AnnotationWinController *sharedController;
 - (void)awakeFromNib {
     sharedController = self;
     self.width = 160;
-    // self.height = 282;
-
+    //self.height = 282;
+    
     [self.panel orderFront:nil];
     [self.panel setLevel:CGShieldingWindowLevel() + 1];
-
     // Make sure panel can float over full screen apps
-    self.panel.collectionBehavior = NSWindowCollectionBehaviorCanJoinAllSpaces;
-
+//  self.panel.collectionBehavior = NSWindowCollectionBehaviorCanJoinAllSpaces;
     [self.panel setStyleMask:NSWindowStyleMaskBorderless];
-
     [self performSelector:@selector(hideWindow) withObject:nil afterDelay:0.01];
     // [self showWindow:NSMakePoint(10, self.height + 10)]; //for dev debug
 }
 
 - (void)showWindow:(NSPoint)origin {
     [self.panel setFrameTopLeftPoint:origin];
-
-    self.panel.alphaValue = 1;
+    self.panel.alphaValue = 1.0;
+    self.panel.collectionBehavior = NSWindowCollectionBehaviorCanJoinAllSpaces;
 }
 
 - (void)hideWindow {
     self.panel.alphaValue = 0;
+    self.panel.collectionBehavior = NSWindowCollectionBehaviorMoveToActiveSpace;
 }
 
 - (void)setAnnotation:(NSString *)annotation {
