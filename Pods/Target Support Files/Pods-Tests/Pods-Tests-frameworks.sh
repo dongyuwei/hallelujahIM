e7d318f90ef52180be6fe7b057c72fc52e529606
Dong Yuwei <newdongyuwei@gmail.com>
2023-09-03 12:37:03 +0800

    trying to fix CI, again

--- Pods/Target Support Files/Pods-Tests/Pods-Tests-frameworks.sh
+++ Pods/Target Support Files/Pods-Tests/Pods-Tests-frameworks.sh
@@ -176,9 +176,11 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/GCDWebServer/GCDWebServer.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MDCDamerauLevenshtein/MDCDamerauLevenshtein.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/GCDWebServer/GCDWebServer.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MDCDamerauLevenshtein/MDCDamerauLevenshtein.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
--- unit-tests.sh
+++ unit-tests.sh
@@ -1,1 +1,2 @@
+xcodebuild clean -workspace hallelujah.xcworkspace/ -scheme Tests
 xcodebuild test CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -workspace hallelujah.xcworkspace/ -scheme Tests
