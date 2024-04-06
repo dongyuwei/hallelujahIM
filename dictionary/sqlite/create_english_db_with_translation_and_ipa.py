0c07932ec103da0a9280fe6c3d78de9c2459f55a
Dong Yuwei <newdongyuwei@gmail.com>
2024-04-06 20:08:11 +0800

    create and query words_with_frequency_and_translation_and_ipa in sqlite3 use Python3

---
+++ dictionary/sqlite/create_english_db_with_translation_and_ipa.py
@@ -0,0 +1,28 @@
+import sqlite3
+import json
+
+with open('../words_with_frequency_and_translation_and_ipa.json', encoding='utf-8') as f:
+    data = json.load(f)
+
+conn = sqlite3.connect('words_with_frequency_and_translation_and_ipa.sqlite3')
+c = conn.cursor()
+
+# Create table
+c.execute('''
+CREATE TABLE IF NOT EXISTS words (
+    word TEXT PRIMARY KEY,
+    frequency INT,
+    translation TEXT,
+    ipa TEXT
+)
+''')
+
+# Insert data
+for word, details in data.items():
+    c.execute('''
+    INSERT INTO words (word, frequency, translation, ipa) VALUES (?, ?, ?, ?)
+    ''', (word, details['frequency'], json.dumps(details['translation']), details['ipa']))
+
+# Commit the changes and close the connection
+conn.commit()
+conn.close()
---
+++ dictionary/sqlite/query_english_db_with_translation_and_ipa.py
@@ -0,0 +1,30 @@
+import sqlite3
+import json
+
+
+def query_words_by_prefix(prefix):
+    with sqlite3.connect('words_with_frequency_and_translation_and_ipa.sqlite3') as conn:
+        c = conn.cursor()
+        # The LIKE operator is case-insensitive in SQLite by default
+        c.execute('SELECT * FROM words WHERE word LIKE ? ORDER BY frequency DESC limit 30', (prefix + '%',))
+        results = c.fetchall()
+        # conn.close()
+        
+        words_details = []
+        for result in results:
+            words_details.append({
+                "word": result[0],
+                "translation": '|'.join(json.loads(result[2])),
+                "ipa": result[3]
+            })
+        return words_details
+
+# Example usage: Find words starting with "pre"
+prefix_match_words = query_words_by_prefix("TEst")
+for word_detail in prefix_match_words:
+    print(111, word_detail)
+
+prefix_match_words2 = query_words_by_prefix("good")
+for word_detail in prefix_match_words2:
+    print(222, word_detail)
+
